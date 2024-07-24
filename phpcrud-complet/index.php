f26230ee288bfe2ef2087157b1b51596ddd96c8d
Laza <laza.andriamampianina67@outlook.com>
2024-07-24 10:45:07 +0200

    base du programme

---
+++ phpcrud-complet/index.php
@@ -0,0 +1,12 @@
+<?php
+$title = "Accueil";
+include "head.php";
+?>
+<div class="content">
+    <h2>Accueil</h2>
+    <p>Bienvenue sur la page d’accueil !</p>
+</div>
+
+<?php
+include "foot.php";
+?>
