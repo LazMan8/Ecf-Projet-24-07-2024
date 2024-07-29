5d7d503bf6146d617f80a171d87af780c6de32c1
LazMan8 <laza.andriamampianina67@outlook.com>
2024-07-29 13:57:32 +0200

    fin mise en point

--- app/views/Partial/header.php
+++ app/views/Partial/header.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/Assets/css/style.css">
-    <title><?= $_GET['page'] ?></title>
+    <title>projet</title>
 </head>
 <header>
         <?php
