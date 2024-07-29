4335d1f09224890aec4c85d91bec90a513dc533e
Eric <ecastellani@outlook.fr>
2024-07-29 14:01:14 +0200

    Harmonisqtion branches

--- app/Views/Partial/header.php
+++ app/Views/Partial/header.php
@@ -1,25 +1,25 @@
-<?php
-?>
+
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GESTION DES HABILITATIONS</title>
-    
-    <link rel="stylesheet" href = "Assets/css/style.css">
+    <link rel="stylesheet" href="/Assets/css/style.css">
+    <title>projet</title>
 </head>
-
-<body>
-<main class ="main"></main>
-    <h1 class="titre">Gestion des habilitations</h1>
-    <nav>
-        <a class="link" href="../app/Views/gestionRole.php">Gestion des rôles</a>
-        <a class="link" href="../app/Views/affectationRole.php">Affectation des rôles</a>
-        <a class="link" href="../app/Views/definitionDroits.php">Définition des droits</a>
-        
-      </nav>
-    
-</body>
+<header>
+        <?php
+        if (isset($_SESSION['id'])) 
+        {
+            ?>
+            <ul>
+                <li>
+                <a href="index.php?page=deconnexion">Se déconnecter</a>
+                </li>
+            </ul>
+            <?php
+        } 
+            ?>
+</header>
 </html>
