12b3b74dafdb6eb4bb51d83fbb64bfd8e4521cc4
Eric <ecastellani@outlook.fr>
2024-07-30 16:42:29 +0200

    maj affectation

--- app/Models/RoleModel.php
+++ app/Models/RoleModel.php
@@ -1,67 +1,57 @@
 <?php
 
-class RoleModel extends ConnexionBD{
+class RoleModel extends ConnexionBD
+{
 
-    public function __construct(){
+    public function __construct()
+    {
         parent::__construct();
     }
 
 
     // Suppression des roles
-    public function deleteRole($idRoleAppli, $idAppli){
-        
-        
+    public function deleteRole($idRoleAppli, $idAppli)
+    {
+
+
         $strQuery = 'DELETE FROM esthabilite WHERE idRoleAppli=? && idAppli = ?';
-        $stmt = $this-> _dataBase->prepare($strQuery);
+        $stmt = $this->_dataBase->prepare($strQuery);
         $stmt->execute([$idRoleAppli, $idAppli]);
 
         $strQuery = 'DELETE FROM roleapplicatif WHERE idRoleAppli=? && idAppli = ?';
-        $stmt = $this-> _dataBase->prepare($strQuery);
+        $stmt = $this->_dataBase->prepare($strQuery);
         $stmt->execute([$idRoleAppli, $idAppli]);
 
         return $stmt->rowCount();
-
-
-
     }
 
-    public function ListRole() {
+    public function ListRole()
+    {
         $strQuery = "SELECT `application`.idAppli, nomAppli,bdAppli,roleapplicatif.idRoleAppli,mdpRoleAppli 
                        FROM roleapplicatif 
                         INNER JOIN `application` ON `application`.idAppli = roleapplicatif.idAppli ";
-                        
-        $dbRoles = $this-> _dataBase->query($strQuery)->fetchAll();
-        foreach($dbRoles as $dbRole) {
-            $roles[] = new RoleEntity($dbRole['idAppli'], $dbRole['idRoleAppli'], $dbRole['mdpRoleAppli'], 
-                         $dbRole['nomAppli'], $dbRole['bdAppli']);
-        } 
-        
+
+        $dbRoles = $this->_dataBase->query($strQuery)->fetchAll();
+        foreach ($dbRoles as $dbRole) {
+            $roles[] = new RoleEntity(
+                $dbRole['idAppli'],
+                $dbRole['idRoleAppli'],
+                $dbRole['mdpRoleAppli'],
+                $dbRole['nomAppli'],
+                $dbRole['bdAppli']
+            );
+        }
+
         // Exécution de la requête et affichage des résultats
         return $roles;
     }
-    
+
     // fonction qui ajoute un role
     public function addRole($idAppli, $idRoleAppli, $mdRoleAppli)
     {
         $strQuery = "INSERT INTO roleapplicatif (idAppli, idRoleAppli, mdpRoleAppli) VALUES (:idAppli, :idRoleAppli, :mdpRoleAppli)";
-        $stmt = $this-> _dataBase->prepare($strQuery);
+        $stmt = $this->_dataBase->prepare($strQuery);
         $stmt->execute([$idAppli, $idRoleAppli, $mdRoleAppli]);
         return $stmt->rowCount();
-        
-
-       
- 
-        }
-
-
-
-
-
-
-
-
-
-
-
-
+    }
 }
--- app/views/affectation.php
+++ app/views/affectation.php
@@ -101,6 +101,7 @@
         </table>
  
     </div>
+    
  
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
  
